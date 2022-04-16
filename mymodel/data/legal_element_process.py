7ac8b199438bd12eba1cd2f4a7ce06d49281063f
PANhuihuihuihui <phjhk@connect.hku.hk>
2022-04-16 21:48:07 +0800

    data prepare

---
+++ mymodel/data/legal_element_process.py
@@ -0,0 +1,45 @@
+# why so many loop?
+# file pattern [ { sentence: "", label: ""}]
+
+# load file
+with codecs.open("/home/huijie/legal/huggface/data_prepare/data/HKLII.json", 'a+') as json_file:
+    with open('/home/xiangwei/classify/divorce.json','r',encoding='utf8')as fp:
+        lines=fp.readlines()
+        for line in lines:
+            data = json.loads(line)
+            temp=[-1]*20
+            for label in data['labels']:
+                index = int(label[2:])-1
+                temp[index]=1
+            data['labels'] = temp + [-1]*40
+            data['class'] = "divorce"
+            json.dump(data, json_file,ensure_ascii=False)
+            json_file.write('\n')
+               
+    with open('/home/xiangwei/classify/labor.json','r',encoding='utf8')as fp:
+        lines=fp.readlines()
+        for line in lines:
+            data = json.loads(line)
+            temp=[-1]*20
+            for label in data['labels']:
+                index = int(label[2:])-1
+                temp[index]=1
+            data['labels'] = [-1]*20 + temp + [-1]*20
+            data['class'] = "labor"
+            json.dump(data, json_file,ensure_ascii=False)
+            json_file.write('\n')
+   
+    with open('/home/xiangwei/classify/loan.json','r',encoding='utf8')as fp:
+        lines=fp.readlines()
+        for line in lines:
+            data = json.loads(line)
+            temp=[-1]*20
+            for label in data['labels']:
+                index = int(label[2:])-1
+                temp[index]=1
+            data['labels'] = [-1]*40 + temp
+            data['class'] = "loan"
+            json.dump(data, json_file,ensure_ascii=False)
+            json_file.write('\n')
+
+
